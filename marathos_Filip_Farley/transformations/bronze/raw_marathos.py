182e131cb92e4bdfe72cc0b7eb999df809cce6be
Farleys46 <filip@farley.se>
2026-05-31 10:52:01 +0000

    silver layer progress
    Almost finished silver_EDA also started on transformation file for OBT

---
+++ marathos_Filip_Farley/transformations/bronze/raw_marathos.py
@@ -0,0 +1,21 @@
+from pyspark import pipelines as dp
+
+BASE_DIR = "/Volumes/marathos/default/raw"
+
+schema = (
+    spark.read.format("csv")
+    .options(header=True, inferschema=True)
+    .load(f"{BASE_DIR}/data/TWO_CENTURIES_OF_UM_RACES.csv")
+    .schema
+ )
+
+@dp.table(
+    name="marathos.bronze.raw_marathos", 
+    comment="Raw marathons data, for Ultra Marathons",
+    table_properties={
+        "delta.columnMapping.mode": "name",
+        "delta.minReaderVersion": "2",
+        "delta.minWriterVersion": "5"
+        })
+def raw_marathos():
+    return spark.readStream.format("csv").options(header=True, encoding="UTF-8").schema(schema).load(f"{BASE_DIR}/data")
