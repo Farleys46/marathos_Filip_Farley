fbdacb010667434454a36c65271c891d961f2aa4
Farleys46 <filip@farley.se>
2026-06-02 12:55:12 +0000

    added marts distance/time

---
+++ marathos_Filip_Farley/transformations/gold/mart_distance_events.sql
@@ -0,0 +1,21 @@
+USE CATALOG marathos;
+USE SCHEMA gold;
+
+CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.mart_distance_events
+COMMENT 'Mart for distance-based events' AS
+SELECT 
+    f.result_id,
+    f.athlete_performance,
+    f.athlete_average_speed,
+    a.athlete_id,
+    a.athlete_age_category,
+    a.athlete_gender,
+    a.athlete_country,
+    e.event_name,
+    e.event_date,
+    e.event_country,
+    e.event_distance_length
+FROM fct_results f
+LEFT JOIN dim_event e ON f.event_id = e.event_id
+LEFT JOIN dim_athlete a ON f.athlete_id = a.athlete_id
+WHERE e.event_type = 'distance';
